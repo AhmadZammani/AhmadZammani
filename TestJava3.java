dc939c5956a280cfe4cbe5254d8b63aa1b4ef97d
Ahmad Farhad <farhad.zammani@gmail.com>
2023-08-14 07:56:39 -0700

    adding new details

---
+++ TestJava3.java
@@ -0,0 +1,10 @@
+package Git_first_Project_Demo;
+
+public class TestJava3 {
+
+	public static void main(String[] args) {
+		System.out.println("Add new class to the designated repository");
+
+	}
+
+}
